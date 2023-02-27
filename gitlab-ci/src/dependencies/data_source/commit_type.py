cacda1ee717844696e14c2ed434409a77c46d747
Chandra Penke <chandrapenke@gmail.com>
2023-02-26 16:05:12 -0800

    Remove gitlab-ci

--- gitlab-ci/src/dependencies/data_source/commit_type.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class CommitType(Enum):
-    MERGE_COMMIT = 1
-    RELEASE_COMMIT = 2
--- gitlab-ci/src/dependencies/scanner/scanner_job_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class ScannerJobType(Enum):
-    MERGE_SCAN = 1
-    RELEASE_SCAN = 2
-    PERIODIC_SCAN = 3
